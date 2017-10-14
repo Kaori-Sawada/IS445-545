375c3ade7b6f67c2e5f4fc4df8deafc87fcd6b9a
Kaori Sawada <kaorisawada@Kaoris-Air.home>
2017-10-14 14:57:37 -0700

    changed some

--- JsTemplate/ExternalCode/app.js
+++ JsTemplate/ExternalCode/app.js
@@ -1,5 +1,6 @@
-const message = prompt('Enter a message')
+const message = prompt('Please enter a number between 2 and 10: ', 0)
 
-// alert('Your input is: ' + message)
+
+alert('Your input is: ' + message)
 const output = document.getElementById("output")
 output.innerHTML = 'Your message is ' + message

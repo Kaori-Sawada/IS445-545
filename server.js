e0141a5bb599e0fb4e9e112acef8fed3eb35ca42
Kaori Sawada <kaorisawada@dhcp-39-44-26.dhcpint.csulb.edu>
2017-10-16 14:16:41 -0700

    added some changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const DEFAULT_PORT = 3377
 const port = process.env.PORT || DEFAULT_PORT
 
 // Serve up content from public directory
-app.use(express.static(__dirname + '/ExternalCode'));
+app.use(express.static(__dirname + '/website'));
 
 console.log(`Server listening at port ${port}`)
 app.listen(port);

c7ee93971ae2b7f9d65a08484eaffb057798d3ff
Kaori Sawada <kaorisawada@Kaoris-Air.home>
2017-11-05 21:45:34 -0800

    final change

--- website/Hw5/app.js
+++ website/Hw5/app.js
@@ -2,12 +2,11 @@ let options = prompt('Choose an option: \n1: Show messages\n2: Add a message\n3:
 let messages = ['message01', 'message02', 'message03', 'message04', 'message05'] ;
 const output = document.getElementById("output")
 const output2 = document.getElementById("output2")
-const output3 = document.getElementById("output3")
 const output4 = document.getElementById("output4")
-let OLlist = document.createElement('ol')
+/*let OLlist = document.createElement('ol') // create numbered list
 OLlist.setAttribute('id', 'numList')
 document.body.appendChild(OLlist)
-let Linum = document.createElement('li') //add numbered list
+let Linum = document.createElement('li') //add numbered list*/
 
 // input 1 
 if (options == 1) {
@@ -23,9 +22,9 @@ if (options == 1) {
 else if (options == 2) {     // input 2
     let newMsg1 = prompt('Enter a new message ', 0)
         messages[messages.length]= newMsg1
-        let newMsg2 = document.createTextNode(newMsg1)  // added msg
-        Linum.appendChild(newMsg2) // add added msg
-        document.getElementById('OLlist').appendChild(Linum) //display the msg in html
+        //let newMsg2 = document.createTextNode(newMsg1)  // added msg
+        //Linum.appendChild(newMsg2) // add added msg
+       // document.getElementById('OLlist').appendChild(Linum) //display the msg in html
         output4.innerHTML = 'The current messages are: ' + '<br>'+ messages.join('<br>')
 }
 

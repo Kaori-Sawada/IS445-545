44280f92b6aa6912333c0c052736e738f4752261
Kaori Sawada <kaorisawada@dhcp-39-45-50.dhcpint.csulb.edu>
2017-11-27 14:34:48 -0800

    finish!!

--- HW6/app.js
+++ HW6/app.js
@@ -1,10 +1,10 @@
 const output = document.getElementById("output")
 const nameInput = document.getElementById("name")
 const candidates = [
-    {name:'Anderson.Paak', likes: '700'},
-    {name:'Bruno Mars', likes: '1000'},
-    {name:'Steve Lacy', likes: '400'},
-    {name:'Erica Badu', likes: '790'},
+    {name:'Anderson.Paak', likes: 700},
+    {name:'Bruno Mars', likes: 1000},
+    {name:'Steve Lacy', likes: 400},
+    {name:'Erica Badu', likes: 790},
     {name: 'Robert Glasper', likes: '800'},
 ]
 output.innerHTML = candidates.likes
@@ -39,15 +39,28 @@ function showList() {
 }
 
 
-function lineClick(e) {
-    let btn = e.target
-    btn = candidates.likes
-    btn++
-    document.getElementById('li').value = btn
-    console.log(btn)
-}
+document.addEventListener('click', lineClick);
+ 
+function lineClick (event){
+    const element = event.target
 
-function incrementLike () {
+    const btnType = element.getAttribute('data-btn-type')
+    const lineIndex = element.getAttribute('data-line-index')
+    const index = parseInt(lineIndex, 10)
+    if (btnType === 'like') {
+        const likes = candidates[index].likes
+        candidates[index].likes = likes + 1
+    } 
+
+    if (btnType === 'delete') {
+        candidates.splice(index, 1)
+    } 
+
+    showList()
+};
+
+
+/* function incrementLike () {
     i = candidates.likes
     i++
-}
+} */

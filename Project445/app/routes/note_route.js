00c5552ac4371fa265d55e866a937a52773bb58d
Kaori Sawada <kaorisawada@dhcp-39-45-237.dhcpint.csulb.edu>
2017-12-18 11:03:31 -0800

    add changes

--- Project445/app/routes/note_route.js
+++ Project445/app/routes/note_route.js
@@ -1,11 +1,18 @@
 //build routes --express are wrapped in a function
 module.exports = function(app, db) {
-    //Make a CREATE route
     app.post('/notes', (req, res) => {
-
-        res.send('Hello')
+    //Make a CREATE route
+    const newUser = {text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(newUser, (err, results) => {
+        if (err) {
+            res.send({'error': ' An error has occurred' });
+        } else {
+            res.send(result.ops[0]);
+        }
     });
+  });
 };
+
 
 
 
